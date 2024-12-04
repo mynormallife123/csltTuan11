027a679e65d633ac5c26257a01e425362fcc079b
Huỳnh Nguyên Phát <hnphat0415@gmail.com>
2024-12-04 13:35:27 +0700

    Update thCslt.cpp
    - getNode got updated
    - addFirst got updated

--- thCslt.cpp
+++ thCslt.cpp
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//update 1:
+//getNode function done
+//addFirst function done
+
 struct PhanSo {
 	int num; //tu so
 	int den; //mau so
@@ -19,22 +23,25 @@ struct list {
 
 
 //khoi khai bao ham
-node* getNode(PhanSo newKey);
-int length(node* head);
-std::ostream& operator<< (std::ostream& outDev, const PhanSo& ps);
-std::istream& operator>> (std::istream& inDev, PhanSo& ps);
-void addFirst(node*& head, node*& tail, PhanSo key);
-void addLast(node*& head, node*& tail, PhanSo &k);
-void insertAfter(node* q, PhanSo k);
-void deleteBegin(node*& head, node*& tail);
-void deleteMiddle(node* &q, node*& head);
-void deleteEnd(node*& head, node*& tail);
-void XayDungDanhSachPhanSo(node*& head, node*& tail);
-void XuatDanhSach(node* head);
-void ThemPhanSo(node*& head, node*& tail, PhanSo& newFrac);
-void ChenPhanSo(node*& head, node*& tail, int& pos, PhanSo& k);
-void XoaPhanSo(node*& head, node*& tail, int &pos);
+node* getNode(void* pNewKey, int dataSize); //done
+int length(node* head); //wip
+std::ostream& operator<< (std::ostream& outDev, const PhanSo& ps); //wip
+std::istream& operator>> (std::istream& inDev, PhanSo& ps); //wip
+void addFirst(node* &pHead, node* &pTail, void* pNodeKey, int dataSize); //done
+void addLast(node*& head, node*& tail, PhanSo &k); //wip
+void insertAfter(node* q, PhanSo k); //wip
+void deleteBegin(node*& head, node*& tail); //wip
+void deleteMiddle(node* &q, node*& head); //wip
+void deleteEnd(node*& head, node*& tail); //wip
+void XayDungDanhSachPhanSo(node*& head, node*& tail); //wip
+void XuatDanhSach(node* head); //wip
+void ThemPhanSo(node*& head, node*& tail, PhanSo& newFrac); //wip
+void ChenPhanSo(node*& head, node*& tail, int& pos, PhanSo& k); //wip
+void XoaPhanSo(node*& head, node*& tail, int &pos); //wip
 //khoi khai bao ham
+
+
+
 
 
 
@@ -44,36 +51,43 @@ std::ostream& operator<< (std::ostream& outDev, const PhanSo& ps) {
 	if (ps.den == 1 || ps.num == 0) outDev << ps.num;
 	else outDev << ps.num << "/" << ps.den;
 	return outDev;
-}
+} //wip
 
 std::istream& operator>> (std::istream& inDev, PhanSo& ps) {
 	inDev >> ps.num >> ps.den;
 	return inDev;
-}
+} //wip
 
-node* getNode(PhanSo newKey) {
-	node* Node;
-	Node = (node*)malloc(sizeof(node));
+node* getNode(void* pNewKey, int dataSize) {
+	node* pNewNode;
+	pNewNode = (node*)malloc(sizeof(node));
 
-	if (Node == nullptr) {
+	if (pNewNode == nullptr) {
 		std::cout << "Thua thay, khong du bu nho de them phan tu moi.";
 		exit(0);
 	}
-	Node->key = newKey;
-	Node->next = nullptr;
-	return Node;
-}
+	pNewNode->key = new char[dataSize];
+	pNewNode->next = nullptr;
+	
+	if (pNewNode->key == nullptr) {
+		delete pNewNode;
+		return nullptr;
+	}
+	memcpy(pNewNode->key, pNewKey, dataSize);
 
-void addFirst(node* &head, node* &tail, PhanSo key) {
-	node* pNodePtr = getNode(key);
-	if (head == nullptr) {
-		head = tail = pNodePtr;
+	return pNewNode;
+} //done
+
+void addFirst(node* &pHead, node* &pTail, void* pNodeKey, int dataSize) {
+	if (pNodekey == nullptr) return;
+	node* pNewHead = getNode(pNodeKey, dataSize);
+
+	if (pNewHead != nullptr) {
+		pNewHead->next = pHead;
+		pHead = pNewHead;
 	}
-	else {
-		pNodePtr->next = head;
-		head = pNodePtr;
-	}
-}
+} //done
+
 
 void addLast(node*& head, node*& tail, PhanSo& k) {
 	node* ptr = getNode(k);
@@ -85,7 +99,7 @@ void addLast(node*& head, node*& tail, PhanSo& k) {
 		tail->next = ptr;
 		tail = ptr;
 	}
-}
+} //wip
 
 
 void deleteBegin(node*& head, node*& tail) {
@@ -99,7 +113,9 @@ void deleteBegin(node*& head, node*& tail) {
 		head = head->next;
 		free(q);
 	}
-}
+} //wip
+
+
 void XayDungDanhSachPhanSo(node*& head, node*& tail) {
 	PhanSo inputFraction; //use for input
 	while (1) {
@@ -109,7 +125,9 @@ void XayDungDanhSachPhanSo(node*& head, node*& tail) {
 			break;
 		addFirst(head, tail, inputFraction);
 	}
-}
+} //wip
+
+
 void XuatDanhSach(node* head) {
 	if (head == nullptr) {
 		std::cout << "Thua thay, danh sach rong\n";
@@ -121,7 +139,8 @@ void XuatDanhSach(node* head) {
 		}
 		std::cout << "\n";
 	}
-}
+} //wip
+
 void ThemPhanSo(node*& head, node*& tail, PhanSo& newFrac) {
 	//them phan so vao cuoi danh sach
 	std::cout << "Moi thay nhap phan so can them vao cuoi danh sach: ";
@@ -134,28 +153,28 @@ void ThemPhanSo(node*& head, node*& tail, PhanSo& newFrac) {
 		tail->next = newLastFrac; //error tail was nullptr
 		tail = newLastFrac;
 	}
-}
+} //wip
 int length(node* head) {
 	//dem so phan tu trong danh sach lien ket
 	node* ptr;
 	int count = 0;
 	for (ptr = head; ptr; ptr = ptr->next) count++;
 	return count;
-}
+} //wip
 
 void insertAfter(node* q, PhanSo k) {
 	node* p;
 	p = getNode(k);
 	p->next = q->next;
 	q->next = p;
-}
+} //wip
 
 void deleteMiddle(node*& q, node*& head) {
 	node* ptr;
 	for (ptr = head; ptr->next != q; ptr = ptr->next);
 	(ptr->next) = q->next;
 	free(q);
-}
+} //wip
 
 void deleteEnd(node*& head, node*& tail) {
 	node* q;
@@ -169,7 +188,7 @@ void deleteEnd(node*& head, node*& tail) {
 		q->next = nullptr;
 	}
 
-}
+} //wip
 
 void ChenPhanSo(node*& head, node*& tail, int& pos, PhanSo& k) {
 	int n, i = 0;
@@ -193,7 +212,7 @@ void ChenPhanSo(node*& head, node*& tail, int& pos, PhanSo& k) {
 		for (q = head; i < pos - 1; i++, q = q->next);
 		insertAfter(q, k);
 	}
-}
+} //wip
 
 
 
@@ -216,7 +235,7 @@ void XoaPhanSo(node*& head, node*& tail, int &pos) {
 		for (q = head, i = 0; i < pos; q = q->next, i++);
 		deleteMiddle(q, head);		
 	}
-}
+} //wip
 void HuyDanhSach(node*& head) {
 	node* p;
 	while (head) {
@@ -224,7 +243,7 @@ void HuyDanhSach(node*& head) {
 		head = head->next;
 		free(p);
 	}
-}
+} //wip
 //khoi dinh nghia ham
 
 int main() {
